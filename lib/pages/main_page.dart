eae649689f448ddba6219091f26ef008edf2af4b
Jan <20jackoza06@gmail.com>
2024-10-21 12:14:08 +0200

    Use routes data in main page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:mariner/theme/colors.dart';
 
-import 'package:mariner/models/section_button_model.dart';
-
 import 'package:mariner/components/dashboard/section_button.dart';
 import 'package:mariner/components/side_menu.dart';
-import 'package:mariner/pages/subsections_page.dart';
+
+import 'package:mariner/data/main_routes.dart';
 
 @RoutePage()
 class MainPage extends StatelessWidget {
@@ -18,53 +16,17 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const List<SectionButtonModel> buttons = [
-      SectionButtonModel(
-          title: 'Członkowie',
-          icon: Icons.person,
-          route: '/members',
-          hasSubSec: true
-      ),
-      SectionButtonModel(
-          title: 'Składki',
-          icon: Icons.monetization_on,
-          route: '/contributions',
-          hasSubSec: true
-      ),
-      SectionButtonModel(
-          title: 'Uprawnienia żeglarskie',
-          icon: Icons.note_add,
-          route: '/sailor_permissions',
-          hasSubSec: true
-      ),
-      SectionButtonModel(
-          title: 'Marina',
-          icon: Icons.waves,
-          route: '/marina',
-          hasSubSec: true
-      ),
-      SectionButtonModel(
-        title: 'Statusy',
-        icon: Icons.star,
-        route: '/statuses'
-      ),
-      SectionButtonModel(
-        title: 'Ustawienia klubu',
-        icon: Icons.settings,
-        route: '/settings'
-      ),
-    ];
-
+    const routes = kMainRoutes;
     final colors = ThemeColors.of(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-            'MARINER',
-            style: TextStyle(
-                color: colors['textPrimary'],
-                fontWeight: FontWeight.w700
-            )
+          'MARINER',
+          style: TextStyle(
+              color: colors['textPrimary'],
+              fontWeight: FontWeight.w700
+          )
         ),
         actions: [
           IconButton(
@@ -73,9 +35,6 @@ class MainPage extends StatelessWidget {
               color: colors['textPrimary'],
             ),
             onPressed: () {
-              // final prefs = await SharedPreferences.getInstance();
-              // prefs.setBool('loggedIn', false);
-              // Navigator.pushReplacementNamed(context, '/login');
               final router = AutoRouter.of(context);
               router.replaceNamed('/logout');
             },
@@ -91,24 +50,17 @@ class MainPage extends StatelessWidget {
             crossAxisSpacing: 16.0,
             mainAxisSpacing: 16.0,
           ),
-          itemCount: buttons.length,
+          itemCount: routes.length,
           itemBuilder: (context, index) {
-            final button = buttons[index];
+            final route = routes[index];
 
             return SectionButton(
-              title: button.title,
-              icon: button.icon,
+              title: route.title,
+              icon: route.icon,
               onPressed: () {
-                // if (button.hasSubSec && button.hasRoute) {
-                //   Navigator.pushNamed(context, SectionsPage.id, arguments: button.route);
-                // } else if (!button.hasSubSec && button.hasRoute) {
-                //   Navigator.pushNamed(context, button.route.toString() );
-                // }
-                if (button.hasRoute) {
-                  final router = AutoRouter.of(context);
-                  print(button.route);
-                  router.pushNamed(button.route!);
-                }
+                final router = AutoRouter.of(context);
+
+                router.pushNamed(route.route);
               },
             );
           },
