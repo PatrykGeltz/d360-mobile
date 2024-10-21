d8ec1a3a47bc98b8c1a093861002ba2143bf3906
Jan <20jackoza06@gmail.com>
2024-10-21 12:25:23 +0200

    Fill section routes with data

--- lib/data/section_routes.dart
+++ lib/data/section_routes.dart
@@ -1,8 +1,26 @@
+import 'package:flutter/material.dart';
+
 import 'package:mariner/models/section_route_model.dart';
 
 const Map<String, List<SectionRouteModel>> kSectionRoutes = {
-  'members': [],
-  'contributions': [],
-  'marina': [],
-  'sailor_permissions': [],
+  '/members': [
+    SectionRouteModel(title: 'Użytkownicy', icon: Icons.groups, route: '/members/users'),
+    SectionRouteModel(title: 'Zaproszenia', icon: Icons.mail, route: '/members/invites'),
+    SectionRouteModel(title: 'Prośby o dołączenie', icon: Icons.insert_invitation, route: '/members/join_requests'),
+    SectionRouteModel(title: 'Role', icon: Icons.perm_contact_calendar_rounded, route: '/members/roles'),
+    SectionRouteModel(title: 'Statusy', icon: Icons.start, route: '/members/statuses')
+  ],
+  '/contributions': [
+    SectionRouteModel(title: 'Skladki', icon: Icons.monetization_on, route: '/contributions/contributions'),
+    SectionRouteModel(title: 'Historia składek', icon: Icons.history, route: '/contributions/history'),
+    SectionRouteModel(title: 'Slownik opłat', icon: Icons.pending_actions, route: '/contributions/dictionary')
+  ],
+  '/marina': [
+    SectionRouteModel(title: 'Miejsca postojowe', icon: Icons.anchor, route: '/marina/parking_places'),
+    SectionRouteModel(title: 'Szafki', icon: Icons.lock_open, route: '/marina/lockers')
+  ],
+  '/sailor_permissions': [
+    SectionRouteModel(title: 'Patenty', icon: Icons.badge, route: '/sailor_permissions/permissions'),
+    SectionRouteModel(title: 'Rodzaje patentow', icon: Icons.document_scanner, route: '/sailor_permissions/permission_types')
+  ],
 };
