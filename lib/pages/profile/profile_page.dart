8dc8e5d7d092054145f85a7bdaccdd456800e76a
Jan <20jackoza06@gmail.com>
2024-10-22 11:44:35 +0200

    Remove import duplicate

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:auto_route/auto_route.dart';
 import 'package:http_parser/http_parser.dart';
-import 'package:mariner/providers/user_provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
