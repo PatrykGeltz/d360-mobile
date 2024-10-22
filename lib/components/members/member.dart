9be99e08c8defd19d6f664383b11be2191635fc4
PatrykGeltz <patryk.geltz07@gmail.com>
2024-10-22 14:41:04 +0200

    connected member list to local server

--- lib/components/members/member.dart
+++ lib/components/members/member.dart
@@ -1,9 +1,23 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'member_detail.dart';
 
 class Member extends StatelessWidget {
-  const Member({super.key, required this.id, required this.name, this.pesel = '* Nie podano *', this.email = '* Nie podano *', this.phone = '* Nie podano *',this.city = '* Nie podano *',this.address = '* Nie podano *',});
+  const Member({
+    super.key,
+    required this.id,
+    required this.name,
+    this.pesel = '* Nie podano *',
+    this.email = '* Nie podano *',
+    this.phone = '* Nie podano *',
+    this.city = '* Nie podano *',
+    this.address = '* Nie podano *',
+    this.date_of_approval = '',
+    this.register_status = '',
+    required this.onDelete,
+  });
 
   final String name;
   final String pesel;
@@ -12,6 +26,45 @@ class Member extends StatelessWidget {
   final String phone;
   final String city;
   final String address;
+  final String date_of_approval;
+  final String register_status;
+  final VoidCallback onDelete; // Callback do wywołania po usunięciu
+
+  Future<void> _deleteMember(BuildContext context) async {
+    var url = 'https://acme-dev.d360.pl/api/v1/members/$id';
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString("access_token");
+
+    if (token == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Błąd autoryzacji')),
+      );
+      return;
+    }
+
+    try {
+      final response = await http.delete(
+        Uri.parse(url),
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Członek usunięty pomyślnie')),
+        );
+        onDelete(); // Wywołaj callback do odświeżenia listy po usunięciu
+      } else {
+        throw Exception('Nie udało się usunąć członka');
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Błąd: ${e.toString()}')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,17 +81,27 @@ class Member extends StatelessWidget {
             onPressed: () {
               showDialog(
                 context: context,
-                builder: (BuildContext context) => MemberDetail(id:id,name: name, pesel: pesel, email: email, phone: phone, address: address, city: city,)
+                builder: (BuildContext context) => MemberDetail(
+                  id: id,
+                  name: name,
+                  pesel: pesel,
+                  email: email,
+                  phone: phone,
+                  address: address,
+                  city: city,
+                  register_status: register_status,
+                  date_of_approval: date_of_approval
+                ),
               );
             },
-            icon: const Icon(Icons.more_horiz)
+            icon: const Icon(Icons.more_horiz),
           ),
           IconButton(
             onPressed: () {
-              print('delete ');
+              _deleteMember(context); // Wywołanie funkcji usuwania
             },
-            icon: const Icon(Icons.delete)
-          )
+            icon: const Icon(Icons.delete),
+          ),
         ],
       ),
     );
--- lib/components/members/member_detail.dart
+++ lib/components/members/member_detail.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mariner/theme/colors.dart';
 
 class MemberDetail extends StatelessWidget {
-  const MemberDetail({super.key, required this.id, required this.name, this.pesel = '* Nie podano *', this.email = '* Nie podano *', this.phone = '* Nie podano *', this.city = '* Nie podano *', this.address = '* Nie podano *'});
+  const MemberDetail({super.key, required this.id, required this.name, this.pesel = '* Nie podano *', this.email = '* Nie podano *', this.phone = '* Nie podano *', this.city = '* Nie podano *', this.address = '* Nie podano *', this.date_of_approval = '* Nie podano *', this.register_status = '* Nie podano *'});
 
   final String name;
   final String id;
@@ -11,6 +11,8 @@ class MemberDetail extends StatelessWidget {
   final String phone;
   final String address;
   final String city;
+  final String register_status;
+  final String date_of_approval;
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +33,11 @@ class MemberDetail extends StatelessWidget {
               Text('Miasto: $city'),
               const SizedBox(height: 5.0,),
               Text('Adres: $address'),
+              const SizedBox(height: 5.0,),
+              Text('Data dołączenia: $date_of_approval'),
+              const SizedBox(height: 5.0,),
+              Text('Status członka: $register_status'),
+              
 
 
             ],
