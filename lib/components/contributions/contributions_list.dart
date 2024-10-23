640a4b15e1eca4edf09bffa797797af2fa4f201e
Jan <20jackoza06@gmail.com>
2024-10-23 10:41:04 +0200

    Add event handlers to contribution actions (delete, edit)

--- lib/components/contributions/contributions_list.dart
+++ lib/components/contributions/contributions_list.dart
@@ -42,8 +42,10 @@ class ContributionsList extends StatelessWidget {
                 onPressed: () {
                   showDialog(
                     context: context,
-                    builder: (context) => const ContributionDetailPopup(
-
+                    builder: (context) => ContributionDetailPopup(
+                      contribution: contribution,
+                      onDelete: () {},
+                      onEdit: () {},
                     )
                   );
                 },
