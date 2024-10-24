489fd8c4ee0cd77f574f6775f6fb2d1d48c96f23
praktykant3 <dawid.domaniecki2@gmail.com>
2024-10-24 11:14:39 +0200

    Added member page

---
+++ lib/pages/modules/members/member_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
+
+@RoutePage()
+class MemberPage extends StatelessWidget {
+  const MemberPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
